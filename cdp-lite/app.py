d2759c7f766ca9977aea8598ea92501505eced89
Paulina Izienicka <paulaiz876@gmail.com>
2025-11-13 20:32:30 +0100

    Fix typo in module and function name

--- cdp-lite/app.py
+++ cdp-lite/app.py
@@ -2,7 +2,7 @@ import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 from modules.importfiles import import_files
 from modules.processdata import process_data
-from modules.rmfanalysis import rmf_analysis
+from modules.rfmanalysis import rfm_analysis
 from modules.openreport import open_report
 from modules.viewlogs import view_logs
 
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     ttk.Label(app, text="Data management panel", font=("Arial", 14, "bold")).pack(pady=15)
     ttk.Button(app, text="📁 Import data", command=import_files, width=25, bootstyle=PRIMARY).pack(pady=5)
     ttk.Button(app, text="⚙️ Process data", command=process_data, width=25, bootstyle=PRIMARY).pack(pady=5)
-    ttk.Button(app, text="🧮 RFM Analysis", command=rmf_analysis, width=25, bootstyle=PRIMARY).pack(pady=5)
+    ttk.Button(app, text="🧮 RFM Analysis", command=rfm_analysis, width=25, bootstyle=PRIMARY).pack(pady=5)
     ttk.Button(app, text="📊 Open Power BI report", command=open_report, width=25, bootstyle=SUCCESS).pack(pady=5)
     ttk.Button(app, text="📄 Processing history", command=view_logs, width=25, bootstyle=INFO).pack(pady=5)
 
