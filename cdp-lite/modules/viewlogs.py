d5c6f1e9ed7d666036de0268ef8645cf388be358
Paulina Izienicka <paulaiz876@gmail.com>
2025-11-14 17:28:42 +0100

    Fix LOGS_DIR path in config and add basic view logs functionality

--- cdp-lite/modules/viewlogs.py
+++ cdp-lite/modules/viewlogs.py
@@ -1,5 +1,10 @@
 from tkinter import messagebox
+import os
+from .config import LOGS_DIR
 
 def view_logs():
-    pass
-    #messagebox.showwarning("File missing", "Log file not found.")
+    logs_path = os.path.join(LOGS_DIR, "log.txt")
+    if os.path.exists(logs_path):
+        os.startfile(logs_path)
+    else:
+        messagebox.showwarning("File missing", "Log file not found.")
